88472f11ad7995ee1081d972a60962bf3619a63a
fkeitxflf <yhl@disanbo.com>
2020-01-13 16:23:01 +0800

    delete ip address

--- src/test/java/cn/chain33/javasdk/client/RpcClientTest.java
+++ src/test/java/cn/chain33/javasdk/client/RpcClientTest.java
@@ -19,7 +19,7 @@ import cn.chain33.javasdk.utils.TransactionUtil;
 
 public class RpcClientTest {
 
-    String ip = "47.75.141.25";
+    String ip = "";
     RpcClient client = new RpcClient(ip, 8801);
 
     String withHoldPrivateKey = "代扣地址私钥，需要有主链代币";
@@ -305,7 +305,7 @@ public class RpcClientTest {
     @Test
     public void convertExecertoAddr() {
         String address;
-        address = client.convertExectoAddr("user.p.demo.game");
+        address = client.convertExectoAddr("user.p.gmchain.none");
         System.out.println(address);
     }
 
@@ -430,16 +430,16 @@ public class RpcClientTest {
         // 主代币则为"",其他为token名
         String coinToken = "";
         // 转账数量为1
-        Long amount = 1 * 100000000L;
+        Long amount = 1 * 10000000L;
         String to = "toAddress";
         //String to = "1CbEVT9RnM5oZhWMj4fxUrJX94VtRotzvs";
         // 本地构造转账交易的payload
         byte[] payload = TransactionUtil.createTransferPayLoad(to, amount, coinToken, note);
         // 签名私私钥，主链上不会扣除本地址下的主链币，所以此地址下可以没有主链币
         String fromAddressPriveteKey = "实际交易签名私钥";
-        //String fromAddressPriveteKey = "0x1ce5a097b01e53d423275091e383a2c3a35d042144bd3bced44194eab2ff18c9";
+        //String fromAddressPriveteKey = "1ce5a097b01e53d423275091e383a2c3a35d042144bd3bced44194eab2ff18c9";
         // 执行器名称，平行链主代币为平行链名称+coins(平行链对应配置文件中的title项)
-        String execer = "user.p.xxchain.coins";
+        String execer = "user.p.sec.coins";
         // 平行链转账时，实际to的地址填在payload中，外层的to地址对应的是合约的地址
         String contranctAddress = client.convertExectoAddr(execer);
         String createTransferTx = TransactionUtil.createTransferTx(fromAddressPriveteKey, contranctAddress, execer, payload);
@@ -449,7 +449,7 @@ public class RpcClientTest {
         // 解析交易
         List<DecodeRawTransaction> decodeRawTransactions = client.decodeRawTransaction(createNoBalanceTx);
         // 代扣交易签名的私钥
-        //String withHoldPrivateKey = "0x3990969DF92A5914F7B71EEB9A4E58D6E255F32BF042FEA5318FC8B3D50EE6E8";
+        //String withHoldPrivateKey = "3990969DF92A5914F7B71EEB9A4E58D6E255F32BF042FEA5318FC8B3D50EE6E8";
         String withHoldPrivateKey = "代扣地址私钥";
         String hexString = TransactionUtil.signDecodeTx(decodeRawTransactions, contranctAddress, fromAddressPriveteKey, withHoldPrivateKey);
         String submitTransaction = client.submitTransaction(hexString);
