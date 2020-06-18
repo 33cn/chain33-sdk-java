dffd3772def3915c104b6d382213e1a8dee3d94d
fkeitxflf <yhl@disanbo.com>
2020-06-18 09:28:40 +0800

    add test cases

--- src/test/java/cn/chain33/javasdk/client/StorageTest.java
+++ src/test/java/cn/chain33/javasdk/client/StorageTest.java
@@ -19,7 +19,7 @@ public class StorageTest {
 	
     String ip = "fd.33.cn";
     RpcClient client = new RpcClient(ip, 1263);
-    
+	    
     String content = "疫情发生后，NPO法人仁心会联合日本湖北总商会等四家机构第一时间向湖北捐赠3800套杜邦防护服，包装纸箱上用中文写有“岂曰无衣，与子同裳”。这句诗词出自《诗经·秦风·无衣》，翻译成白话的意思是“谁说我们没衣穿？与你同穿那战裙”。不料，这句诗词在社交媒体上引发热议，不少网民赞叹日本人的文学造诣。实际上，NPO法人仁心会是一家在日华人组织，由在日或有留日背景的医药保健从业者以及相关公司组成的新生公益组织。NPO法人仁心会事务局告诉环球时报-环球网记者，由于第一批捐赠物资是防护服，“岂曰无衣，与子同裳”恰好可以表达海外华人华侨与一线医护人员共同战胜病毒的同仇敌忾之情，流露出对同胞的守护之爱。";
     
 	/**
@@ -136,7 +136,7 @@ public class StorageTest {
 //		JSONObject resultJson = client.queryStorage("0x8c97bb1659c54c2c95d93d4b0c7423cf604c485b0e534b7bc1aada2c45be5c39");
 //		JSONObject resultJson = client.queryStorage("0xa69815e1fe52ba8787cb710a236127299f7d15798c59ec9ed350d1342d7d256e");
 //		JSONObject resultJson = client.queryStorage("0x95c8ba7a818e94349049e4de0f9259bc6cfd9429ed4af9ede38262ff7f974a7c");
-		JSONObject resultJson = client.queryStorage("存证hash");
+		JSONObject resultJson = client.queryStorage("0x401f043696500030d49a511505b5c703e943382082b1154880e753acacb3d443");
 		
 		JSONObject resultArray;
         if (resultJson.containsKey("linkStorage")) {
