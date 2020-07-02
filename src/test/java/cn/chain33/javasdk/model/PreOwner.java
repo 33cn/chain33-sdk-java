bda6dd6b43de3f0bac50ee3c46c32d6746782e05
fkeitxflf <yhl@disanbo.com>
2020-07-02 19:28:24 +0800

    增加用例

--- src/test/java/cn/chain33/javasdk/model/PreOwner.java
+++ src/test/java/cn/chain33/javasdk/model/PreOwner.java
@@ -6,8 +6,16 @@ import cn.chain33.javasdk.model.pre.KeyFrag;
 import cn.chain33.javasdk.utils.*;
 
 import javax.xml.bind.DatatypeConverter;
+
+import org.junit.Test;
+
 import java.io.UnsupportedEncodingException;
 
+/**
+ * 数据拥有者将数据加密上链，并且将
+ * @author fkeit
+ *
+ */
 public class PreOwner {
 
     // 代理重加密节点
@@ -22,45 +30,44 @@ public class PreOwner {
     static RpcClient chain33Client = new RpcClient("http://139.196.201.120:8901");
 
     // 数据所有者私钥
-    static String OwnerPrivateKey = "4247776e69fec9c72b03812302b98de6663de43ad9fe3674d953079fceea850c";
+    static String OwnerPrivateKey = "2974c5c4cae6f6336923cc834b8c7c946bf5693c56036ccc9f5204724c4832fb";
 
-    // 数据授权人公钥
-    static String RecipientPubKey = "028ad2f80aa90f7182a0519b09f5efbbfc5eb3fae42f99986cd5a9c64ea4c4ced2";
 
     // 代理节点公钥，用于身份验证，不参与重加密和加解密算法
     static String ServerPub = "0x02005d3a38feaff00f1b83014b2602d7b5b39506ddee7919dd66539b5428358f08";
+    
+    // 分4片
+    static int numSplit = 4;
+    
+    // 门限是3， 也就是通过3个分片就可以恢复私钥
+    static int threshold = 3;
 
     // 待加密的数据
     static String content = "Chain33 代理重加密测试";
 
-    public static void preEncrypt(int numSplit, int threshold) {
+    /**
+     * 代理重加密秘钥分片存储到代理重加密服务器，同时数据加密上链存储
+     * @param numSplit
+     * @param threshold
+     */
+    @Test
+    public void preEncrypt() {
         AccountInfo alice = new AccountInfo();
         alice.setPrivateKey(OwnerPrivateKey);
         alice.setPublicKey(TransactionUtil.getHexPubKeyFromPrivKey(OwnerPrivateKey));
-
+        
+        
         // 生成对称秘钥
         EncryptKey encryptKey = PreUtils.GenerateEncryptKey(HexUtil.fromHexString(alice.getPublicKey()));
         System.out.println(DatatypeConverter.printHexBinary(encryptKey.getShareKey()));
 
-        // 生成重加密密钥分片
-        KeyFrag[] kFrags = new KeyFrag[numSplit];
-        try {
-            kFrags = PreUtils.GenerateKeyFragments(HexUtil.fromHexString(alice.getPrivateKey()),
-                    HexUtil.fromHexString(RecipientPubKey), numSplit, threshold);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // 数据被授权人公钥：授权Bob可以看
+        String RecipientBobPubKey = "028bacacbff6c2a040d1e6ec10834470419dafb698be977d762fbba75e2e46aee9";
+        uploadKey(encryptKey, alice, RecipientBobPubKey);
 
-        // 密钥分片发送到代理节点
-        String dhProof = PreUtils.ECDH(ServerPub, alice.getPrivateKey());
-        for(int i = 0; i < preClient.length; i++) {
-            boolean result = preClient[i].sendKeyFragment(alice.getPublicKey(), RecipientPubKey, encryptKey.getPubProofR(),
-                    encryptKey.getPubProofU(), 100, dhProof, kFrags[i]);
-            if (!result) {
-                System.out.println("sendKeyFragment failed");
-                return;
-            }
-        }
+        // 数据被授权人公钥：授权Tom可以看
+        String RecipientTomPubKey = "02d594b686be7273cbeede787ce687304e681f8198610c5e88b26f489f7d4e3ec7";
+        uploadKey(encryptKey, alice, RecipientTomPubKey);
 
         // 数据加密
         byte[] iv = AesUtil.generateIv();
@@ -77,12 +84,36 @@ public class PreOwner {
         }
 
         String txEncode = StorageUtil.createEncryptNotaryStorage(cipher.getBytes(), contentHash, iv,
-                "chain33-storage-key-pre-"+kFrags[0].getPrecurPub(), "", "storage", OwnerPrivateKey);
+                "chain33-storage-key-pre-alice", "", "storage", OwnerPrivateKey);
         String submitTransaction = chain33Client.submitTransaction(txEncode);
         System.out.println(submitTransaction);
     }
 
-    public static void main(String args[]) {
-        preEncrypt(4, 2);
+    /**
+     * 分片并上传加密私钥
+     * 
+     * @param encryptKey
+     * @param alice
+     */
+    private void uploadKey(EncryptKey encryptKey, AccountInfo alice, String pubkey) {
+        // 生成重加密密钥分片
+        KeyFrag[] kFrags = new KeyFrag[numSplit];
+        try {
+            kFrags = PreUtils.GenerateKeyFragments(HexUtil.fromHexString(alice.getPrivateKey()),
+                    HexUtil.fromHexString(pubkey), numSplit, threshold);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 密钥分片发送到代理节点
+        String dhProof = PreUtils.ECDH(ServerPub, alice.getPrivateKey());
+        for(int i = 0; i < preClient.length; i++) {
+            boolean result = preClient[i].sendKeyFragment(alice.getPublicKey(), pubkey, encryptKey.getPubProofR(),
+                    encryptKey.getPubProofU(), 100, dhProof, kFrags[i]);
+            if (!result) {
+                System.out.println("sendKeyFragment failed");
+                return;
+            }
+        }
     }
 }
--- src/test/java/cn/chain33/javasdk/model/PreRecipient.java
+++ src/test/java/cn/chain33/javasdk/model/PreRecipient.java
@@ -7,46 +7,49 @@ import com.alibaba.fastjson.JSONObject;
 
 import javax.xml.bind.DatatypeConverter;
 
+import org.junit.Test;
+
 public class PreRecipient {
 
     // 代理重加密节点
     static RpcClient[]  preClient = new RpcClient[]{
-            new RpcClient("http://192.168.0.155:11801"),
-            new RpcClient("http://192.168.0.155:11802"),
+            new RpcClient("http://139.196.201.120:11801"),
+            new RpcClient("http://139.196.201.141:11801"),
+            new RpcClient("http://139.196.201.241:11801"),
+            new RpcClient("http://139.196.201.49:11801"),
     };
 
     // 区块链节点
-    static RpcClient chain33Client = new RpcClient("http://192.168.0.155:8801");
+    static RpcClient chain33Client = new RpcClient("http://139.196.201.120:8901");
 
     // 数据所有者公钥
-    static String OwnerPubKey = "02b009d9388d452677e4c00535ee60a0266f2688bb6c68cdd81543bc4b5d6adb80";
+    static String OwnerPubKey = "03aeba7f887ac8978eb878d0a45e3938361604c3db0d2442695f3f226e9d5f5567";
 
-    // 被授权人私钥
-    static String RecipientPrivateKey = "6e955c1dc32e76394942233eee23b466313a61231a34fde6bc83815f4a29eb9a";
+    // 被授权人私钥,Bob的私钥
+    static String RecipientBobPrivateKey = "407ba5d402e8760dab6f08b1743143eb31edb978d329f241f97ad71028d2cc3f";
+    
+    // 被授权人私钥,Tom的私钥
+    static String RecipientTomPrivateKey = "386fdbddbda034ed719321a723ed057542879199e18309cfc09e4264b3237833";
+    
+    // 被授权人私钥,James的私钥
+    static String RecipientJamesPrivateKey = "41902240b8aed527841dd0cf257b9cab3e26638f3eb5c93c1f202e9af925b743";
+    
+    // 门限是3， 也就是通过3个分片就可以恢复私钥
+    static int threshold = 3;
 
-    public static void preDecrypt(int threshold) {
+    /**
+     * Bob解密
+     */
+    @Test
+    public void preDecryptBob() {
         AccountInfo bob = new AccountInfo();
-        bob.setPrivateKey(RecipientPrivateKey);
-        bob.setPublicKey(TransactionUtil.getHexPubKeyFromPrivKey(RecipientPrivateKey));
+        bob.setPrivateKey(RecipientBobPrivateKey);
+        bob.setPublicKey(TransactionUtil.getHexPubKeyFromPrivKey(RecipientBobPrivateKey));
 
-        // 申请重加密，需要两边的公钥
-        ReKeyFrag[] reKeyFrags = new ReKeyFrag[threshold];
-        for(int i = 0; i < threshold; i++) {
-            reKeyFrags[i] = preClient[i].reencrypt(OwnerPubKey, bob.getPublicKey());
-        }
-
-        // 解密对称密钥，需要被授权人私钥
-        byte[] shareKeyBob;
-        try {
-            shareKeyBob = PreUtils.AssembleReencryptFragment(HexUtil.fromHexString(bob.getPrivateKey()), reKeyFrags);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        }
-        System.out.println(DatatypeConverter.printHexBinary(shareKeyBob));
+        byte[] shareKeyBob = downloadKey(bob);
 
         // 从链上获取密文
-        JSONObject resultJson = chain33Client.queryStorage("chain33-storage-key-pre-"+reKeyFrags[0].getPrecurPub());
+        JSONObject resultJson = chain33Client.queryStorage("chain33-storage-key-pre-alice");
         JSONObject resultArray = resultJson.getJSONObject("encryptStorage");
         String content = resultArray.getString("encryptContent");
         byte[] fromHexString = HexUtil.fromHexString(content);
@@ -56,8 +59,75 @@ public class PreRecipient {
         String text = AesUtil.decrypt(new String(fromHexString), HexUtil.toHexString(shareKeyBob));
         System.out.println(text);
     }
+    
+    /**
+     * Tom解密
+     */
+    @Test
+    public void preDecryptTom() {
+        AccountInfo tom = new AccountInfo();
+        tom.setPrivateKey(RecipientTomPrivateKey);
+        tom.setPublicKey(TransactionUtil.getHexPubKeyFromPrivKey(RecipientTomPrivateKey));
 
-    public static void main(String args[]) {
-        preDecrypt(2);
+        byte[] shareKeyTom = downloadKey(tom);
+
+        // 从链上获取密文
+        JSONObject resultJson = chain33Client.queryStorage("chain33-storage-key-pre-alice");
+        JSONObject resultArray = resultJson.getJSONObject("encryptStorage");
+        String content = resultArray.getString("encryptContent");
+        byte[] fromHexString = HexUtil.fromHexString(content);
+        System.out.println(new String(fromHexString));
+
+        // 解密
+        String text = AesUtil.decrypt(new String(fromHexString), HexUtil.toHexString(shareKeyTom));
+        System.out.println(text);
+    }
+    
+    /**
+     * James解密
+     */
+    @Test
+    public void preDecryptJames() {
+        AccountInfo james = new AccountInfo();
+        james.setPrivateKey(RecipientBobPrivateKey);
+        james.setPublicKey(TransactionUtil.getHexPubKeyFromPrivKey(RecipientJamesPrivateKey));
+
+        byte[] shareKeyJames = downloadKey(james);
+
+        // 从链上获取密文
+        JSONObject resultJson = chain33Client.queryStorage("chain33-storage-key-pre-alice");
+        JSONObject resultArray = resultJson.getJSONObject("encryptStorage");
+        String content = resultArray.getString("encryptContent");
+        byte[] fromHexString = HexUtil.fromHexString(content);
+        System.out.println(new String(fromHexString));
+
+        // 解密
+        String text = AesUtil.decrypt(new String(fromHexString), HexUtil.toHexString(shareKeyJames));
+        System.out.println(text);
+    }
+
+    /**
+     * 从代理重加密节点下载私钥匙分片，并组成解密私钥匙
+     * 
+     * @param accountInfo
+     * @return
+     */
+    public  byte[] downloadKey(AccountInfo accountInfo) {
+        // 申请重加密，需要两边的公钥
+        ReKeyFrag[] reKeyFrags = new ReKeyFrag[threshold];
+        for(int i = 0; i < threshold; i++) {
+            reKeyFrags[i] = preClient[i].reencrypt(OwnerPubKey, accountInfo.getPublicKey());
+        }
+
+        // 解密对称密钥，需要被授权人私钥
+        byte[] shareKeyBob;
+        try {
+            shareKeyBob = PreUtils.AssembleReencryptFragment(HexUtil.fromHexString(accountInfo.getPrivateKey()), reKeyFrags);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        System.out.println(DatatypeConverter.printHexBinary(shareKeyBob));
+        return shareKeyBob;
     }
 }
