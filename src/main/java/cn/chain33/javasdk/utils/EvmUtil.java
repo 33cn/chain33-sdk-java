43c0571ea3bf18a638a90e0d627f586cc016854a
pengjun <pengj@disanbo.com>
2020-10-23 17:18:39 +0800

    add evm api

---
+++ src/main/java/cn/chain33/javasdk/utils/EvmUtil.java
@@ -0,0 +1,106 @@
+package cn.chain33.javasdk.utils;
+
+import cn.chain33.javasdk.model.protobuf.EvmService;
+import cn.chain33.javasdk.model.protobuf.TransactionProtoBuf;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+public class EvmUtil {
+
+    public static byte[] execer = "evm".getBytes();
+
+    public static String prefix = "user.evm.";
+    /**
+     *
+     * @description 部署合约
+     * @param code  合约代码内容
+     * @param note  注释
+     * @param alias 合约别名
+     * @param abi   合约abi内容
+     * @param privateKey 签名私钥
+     * @return  hash，即合约名
+     *
+     */
+    public static String createEvmContract(byte[] code, String note, String alias, String abi, String privateKey) {
+        EvmService.EVMContractAction.Builder evmActionBuilder = EvmService.EVMContractAction.newBuilder();
+        evmActionBuilder.setCode(ByteString.copyFrom(code));
+        evmActionBuilder.setAbi(abi);
+        evmActionBuilder.setNote(note);
+        evmActionBuilder.setAlias(alias);
+        EvmService.EVMContractAction evmContractAction = evmActionBuilder.build();
+
+        String createTxWithoutSign = TransactionUtil.createTxWithoutSign(execer, evmContractAction.toByteArray(),
+                TransactionUtil.DEFAULT_FEE, 0);
+        byte[] fromHexString = HexUtil.fromHexString(createTxWithoutSign);
+        TransactionProtoBuf.Transaction parseFrom = null;
+        try {
+            parseFrom = TransactionProtoBuf.Transaction.parseFrom(fromHexString);
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+        TransactionProtoBuf.Transaction signProbuf = TransactionUtil.signProbuf(parseFrom, privateKey);
+        String hexString = HexUtil.toHexString(signProbuf.toByteArray());
+        return hexString;
+    }
+
+    /**
+     *
+     * @description  调用合约
+     * @param code   合约代码内容
+     * @param note   注释
+     * @param amount 转账金额
+     * @param abi    合约abi内容
+     * @param contractName 合约名
+     * @param privateKey 签名私钥
+     * @return  hash
+     *
+     */
+    public static String callEvmContract(byte[] code, String note, long amount, String abi, String contractName, String privateKey) {
+        EvmService.EVMContractAction.Builder evmActionBuilder = EvmService.EVMContractAction.newBuilder();
+        evmActionBuilder.setCode(ByteString.copyFrom(code));
+        evmActionBuilder.setAbi(abi);
+        evmActionBuilder.setNote(note);
+        evmActionBuilder.setAmount(amount);
+        EvmService.EVMContractAction evmContractAction = evmActionBuilder.build();
+
+        String createTxWithoutSign = TransactionUtil.createTxWithoutSign((prefix + contractName).getBytes(), evmContractAction.toByteArray(),
+                TransactionUtil.DEFAULT_FEE, 0);
+        byte[] fromHexString = HexUtil.fromHexString(createTxWithoutSign);
+        TransactionProtoBuf.Transaction parseFrom = null;
+        try {
+            parseFrom = TransactionProtoBuf.Transaction.parseFrom(fromHexString);
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+        TransactionProtoBuf.Transaction signProbuf = TransactionUtil.signProbuf(parseFrom, privateKey);
+        String hexString = HexUtil.toHexString(signProbuf.toByteArray());
+        return hexString;
+    }
+
+
+    /**
+     *
+     * @description 销毁合约
+     * @param contractName 合约名
+     * @param privateKey 签名私钥
+     * @return  hash
+     *
+     */
+    public static String destroyEvmContract(String contractName, String privateKey) {
+        EvmService.EVMContractAction.Builder evmActionBuilder = EvmService.EVMContractAction.newBuilder();
+        EvmService.EVMContractAction evmContractAction = evmActionBuilder.build();
+
+        String createTxWithoutSign = TransactionUtil.createTxWithoutSign((prefix + contractName).getBytes(), evmContractAction.toByteArray(),
+                TransactionUtil.DEFAULT_FEE, 0);
+        byte[] fromHexString = HexUtil.fromHexString(createTxWithoutSign);
+        TransactionProtoBuf.Transaction parseFrom = null;
+        try {
+            parseFrom = TransactionProtoBuf.Transaction.parseFrom(fromHexString);
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+        TransactionProtoBuf.Transaction signProbuf = TransactionUtil.signProbuf(parseFrom, privateKey);
+        String hexString = HexUtil.toHexString(signProbuf.toByteArray());
+        return hexString;
+    }
+}
---
+++ src/test/java/cn/chain33/javasdk/model/EvmTest.java
@@ -0,0 +1,45 @@
+package cn.chain33.javasdk.model;
+
+import cn.chain33.javasdk.client.RpcClient;
+import cn.chain33.javasdk.utils.EvmUtil;
+import cn.chain33.javasdk.utils.HexUtil;
+import org.junit.Test;
+
+public class EvmTest {
+
+    RpcClient client = new RpcClient("127.0.0.1", 8801);
+
+    @Test
+    public void testEvmContract() throws InterruptedException {
+        String privateKey = "0x85bf7aa29436bb186cac45ecd8ea9e63e56c5817e127ebb5e99cd5a9cbfe0f23";
+
+        String code = "0x608060405234801561001057600080fd5b506298967f60005560ac806100266000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c806360fe47b11460375780636d4ce63c146053575b600080fd5b605160048036036020811015604b57600080fd5b5035606b565b005b60596070565b60408051918252519081900360200190f35b600055565b6000549056fea26469706673582212206850c96ceb3091ba2b0454750fbb02238fe0e3765327ec62c47ef4acf0b3ff2b64736f6c63430006000033";
+
+        String abi = "[{\"inputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"inputs\":[],\"name\":\"get\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"x\",\"type\":\"uint256\"}],\"name\":\"set\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]";
+
+        // 部署合约
+        String txEncode = EvmUtil.createEvmContract(HexUtil.fromHexString(code), "", "evm-sdk-test", abi, privateKey);
+        String submitTransaction = client.submitTransaction(txEncode);
+        String contractName = submitTransaction;
+        System.out.println(submitTransaction);
+        Thread.sleep(1000);
+
+        // 调用合约
+        txEncode = EvmUtil.callEvmContract("".getBytes(),"", 0, "get()", contractName, privateKey);
+        submitTransaction = client.submitTransaction(txEncode);
+        System.out.println(submitTransaction);
+        Thread.sleep(1000);
+
+        // 销毁合约
+        txEncode = EvmUtil.destroyEvmContract(contractName, privateKey);
+        submitTransaction = client.submitTransaction(txEncode);
+        System.out.println(submitTransaction);
+        Thread.sleep(1000);
+
+        // 再次调用合约
+        txEncode = EvmUtil.callEvmContract("".getBytes(),"", 0, "get()", contractName, privateKey);
+        submitTransaction = client.submitTransaction(txEncode);
+        System.out.println(submitTransaction);
+    }
+
+}
