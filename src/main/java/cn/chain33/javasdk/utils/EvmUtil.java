391926885e023862e35e2738a3d5590ffaeb6893
fkeitxflf <yhl@disanbo.com>
2020-11-27 16:57:18 +0800

    add evm test cases

--- src/main/java/cn/chain33/javasdk/utils/EvmUtil.java
+++ src/main/java/cn/chain33/javasdk/utils/EvmUtil.java
@@ -76,6 +76,30 @@ public class EvmUtil {
        return hexString;
    }
     
+    /**
+    *
+    * @description 部署合约（平行链采用代扣的情况下调用）
+    * @param code  合约代码内容
+    * @param note  注释
+    * @param alias 合约别名
+    * @param abi   合约abi内容
+    * @param privateKey 签名私钥
+    * @return  hash，即合约名
+    *
+    */
+   public static String createEvmContractWithhold(byte[] code, String note, String alias, String abi, String privateKey, String execer, String contranctAddress) {
+       EvmService.EVMContractAction.Builder evmActionBuilder = EvmService.EVMContractAction.newBuilder();
+       evmActionBuilder.setCode(ByteString.copyFrom(code));
+       evmActionBuilder.setAbi(abi);
+       evmActionBuilder.setNote(note);
+       evmActionBuilder.setAlias(alias);
+       EvmService.EVMContractAction evmContractAction = evmActionBuilder.build();
+       
+       String createTransferTx = TransactionUtil.createTransferTx(privateKey, contranctAddress, execer, evmContractAction.toByteArray(), TransactionUtil.PARA_CREATE_EVM_FEE);
+
+       return createTransferTx;
+   }
+    
 
     /**
      * 
@@ -111,7 +135,6 @@ public class EvmUtil {
         return hexString;
     }
     
-    
     /**
     * 
     * @description  调用合约（平行链的情况下调用，要传paraName（平行链名称））
@@ -144,6 +167,31 @@ public class EvmUtil {
        TransactionProtoBuf.Transaction signProbuf = TransactionUtil.signProbuf(parseFrom, privateKey);
        String hexString = HexUtil.toHexString(signProbuf.toByteArray());
        return hexString;
+   }
+    
+
+   /**
+    * @description  调用合约（平行链采用代扣的情况下调用）
+    * @param code
+    * @param note
+    * @param amount
+    * @param abi
+    * @param contractName
+    * @param privateKey
+    * @param contranctAddress
+    * @return
+    */
+   public static String callEvmContractWithhold(byte[] code, String note, long amount, String abi, String contractName, String privateKey, String contranctAddress) {
+       EvmService.EVMContractAction.Builder evmActionBuilder = EvmService.EVMContractAction.newBuilder();
+       evmActionBuilder.setCode(ByteString.copyFrom(code));
+       evmActionBuilder.setAbi(abi);
+       evmActionBuilder.setNote(note);
+       evmActionBuilder.setAmount(amount);
+       EvmService.EVMContractAction evmContractAction = evmActionBuilder.build();
+
+       String createTransferTx = TransactionUtil.createTransferTx(privateKey, contranctAddress, contractName, evmContractAction.toByteArray(), TransactionUtil.PARA_CALL_EVM_FEE);
+
+       return createTransferTx;
    }
 
 
