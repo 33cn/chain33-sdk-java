235690ba180bbdfc83eca99618ff01cb725af9ac
szh <szh@disanbo.com>
2021-01-21 10:32:40 +0800

    push注册和重新激活说明

--- src/test/java/cn/chain33/javasdk/model/PushTest.java
+++ src/test/java/cn/chain33/javasdk/model/PushTest.java
@@ -9,6 +9,7 @@ import cn.chain33.javasdk.model.rpcresult.BooleanResult;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class PushTest {
     // 联盟链节点IP
     String mainIp = "主链IP";
@@ -24,6 +25,20 @@ public class PushTest {
 
     /**
      *  添加推送url
+     *  1、分别注册区块（区块头）推送服务或者合约回执推送服务，注册成功之后就开始推送;
+     *  注册时使用rpc接口Chain33.AddPushSubscribe进行注册，一旦通过name完成注册，其他订阅用户就不能使用相同的名字进行订阅;
+     *  注册用户数最大上限为100个，超过100个，不能继续注册;
+     *  必须保持一致，不然会出错；
+     *  lastSequence=0，lastHeight=0，lastBlockHash=“”，从零开始推送。
+     *  2、重新激活
+     *  当连续推送3次失败之后，就会停止向该用户进行推送；
+     *  如果接收应用程序重启后，需要继续接收数据，则直接通过原有注册信息激活即可，推送服务就会从上次推送成功处，继续推送;
+     *  当注册的名字name相同，不管url是否相同，会有以下几种情况，并做不同的处理:
+     *  - URL不同
+     *  提示该name已经被注册,注册失败；
+     *  - URL相同
+     *  如果推送已经停止，则重新开始推送；
+     *  如果推送正常，则继续推送；
      */
     @Test
     public void push(){
